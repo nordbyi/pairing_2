2533dfb2af8d79726006291ae4952f2f8bff548b
Ian Nordby <ian.c.nordby@gmail.com>
2022-09-21 16:45:25 -0600

    Add destinations array and utilize pop method

--- friends.js
+++ friends.js
@@ -3,3 +3,6 @@ console.log(arrays)
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+var destinations = ["New York", "California", "Rhode Island", "Hawaii", "Oregon"]
+destinations.pop();
+console.log(destinations);
